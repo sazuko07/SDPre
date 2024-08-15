e6f7e7def77e294043db699f393449cf8c81394a
sazuko07 <sazu07@gmail.com>
2024-08-15 12:52:30 -0500

    finished Objective1Lab2

---
+++ Objective1Lab2.java
@@ -0,0 +1,9 @@
+public class Objective1Lab2 {
+  public static void main(String[] args) {
+	  
+      System.out.println("Grocery List:");
+	  System.out.println("1. Eggs");
+	  System.out.println("2. Bacon");
+	  System.out.println("3. Sausage");
+	  }
+}  
